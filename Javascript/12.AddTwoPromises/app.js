146a6127706e604bd4b58dc33d98a738374a06db
Shreyanshs0ni <shreyanshsoni07@gmail.com>
2025-05-28 21:43:17 +0530

    2 java script question done

---
+++ Javascript/12.AddTwoPromises/app.js
@@ -0,0 +1,6 @@
+const addTwoPromises = async (promise1, promise2) => {
+  const [a, b] = await Promise.all([promise1, promise2]);
+  return a + b;
+};
+
+addTwoPromises(Promise.resolve(2), Promise.resolve(2)).then(console.log); // 4
