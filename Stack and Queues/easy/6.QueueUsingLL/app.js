e8f394279c7f508a5c0426dc4f29d5d32f6cc732
Shreyanshs0ni <shreyanshsoni07@gmail.com>
2025-08-25 18:15:25 +0530

    queue using ll done

--- Stack and Queues/easy/6.QueueUsingLL/app.js
+++ Stack and Queues/easy/6.QueueUsingLL/app.js
@@ -9,11 +9,43 @@ class Queue {
   constructor() {
     this.front = null;
     this.rear = null;
+    this.size = 0;
   }
 
-  enqueue(value) {}
+  enqueue(value) {
+    const newNode = new Node(value);
+    if (this.isEmpty()) {
+      this.rear = this.front = newNode;
+    } else {
+      this.rear.next = newNode;
+      this.rear = newNode;
+    }
+    this.size++;
+  }
 
-  dequeue() {}
-  peek() {}
-  isEmpty() {}
+  dequeue() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    const removedValue = this.front.value;
+    this.front = this.front.next;
+    if (this.front === null) {
+      //queue became empty
+      this.rear = null;
+    }
+    this.size--;
+    return removedValue;
+  }
+  peek() {
+    return this.isEmpty() ? null : this.front.value;
+  }
+  isEmpty() {
+    return this.front === null && this.rear === null;
+  }
 }
+
+const q = new Queue();
+q.enqueue(5);
+q.enqueue(10);
+q.dequeue();
+console.log(q.peek());
