8cc859111e2a57cefd43056964c06eec40bb538d
Shreyanshs0ni <shreyanshsoni07@gmail.com>
2025-08-28 17:14:16 +0530

    fibonacci is done

---
+++ Dynamic Programming/easy/2.Fibonacci/app.js
@@ -0,0 +1,14 @@
+const fib = (n) => {
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+
+  let a = 0,
+    b = 1;
+
+  for (let i = 2; i <= n; i++) {
+    [a, b] = [b, a + b];
+  }
+  return b;
+};
+
+console.log(fib(4))
