2874ef32e909cc60af3f04c791287a9c841bc96b
Shreyanshs0ni <shreyanshsoni07@gmail.com>
2025-09-02 19:07:40 +0530

    scoreOfString done

---
+++ String/easy/5.TheScoreOfString/app.js
@@ -0,0 +1,11 @@
+const scoreOfString = (s) => {
+  let score = 0;
+
+  for (let i = 1; i < s.length; i++) {
+    score += Math.abs(s.charCodeAt(i) - s.charCodeAt(i - 1));
+  }
+
+  return score;
+};
+
+console.log(scoreOfString("zaz"));
