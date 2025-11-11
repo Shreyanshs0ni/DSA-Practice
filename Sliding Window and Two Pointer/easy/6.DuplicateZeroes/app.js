8407dbd61abfb2bd61d40042bb0aa9d1659ac317
Shreyansh <shreyanshsoni07@gmail.com>
2025-11-11 11:12:53 +0530

    duplicate Zeroes completed

---
+++ Sliding Window and Two Pointer/easy/6.DuplicateZeroes/app.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} arr
+ * @return {void} Do not return anything, modify arr in-place instead.
+ */
+var duplicateZeros = function (arr) {
+  let zeroes = 0;
+  let n = arr.length;
+  for (let num of arr) {
+    if (num === 0) zeroes++;
+  }
+  let i = n - 1;
+  let j = n + zeroes - 1;
+
+  while (i >= 0) {
+    if (arr[i] !== 0) {
+      if (j < n) arr[j] = arr[i];
+    } else {
+      if (j < n) arr[j] = 0;
+      j--;
+      if (j < n) arr[j] = 0;
+    }
+    i--;
+    j--;
+  }
+};
