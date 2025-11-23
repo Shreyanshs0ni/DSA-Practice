af740ff9c4c1d40afb862382b720908a9d4e565d
Shreyansh <shreyanshsoni07@gmail.com>
2025-11-23 15:13:23 +0530

    rename file

--- Stack and Queues/easy/19.Baseball game/app.js
+++ Stack and Queues/easy/19.Baseball game/app.js
@@ -28,3 +28,33 @@ var calPoints = function (operations) {
 };
 
 console.log(calPoints(["5", "2", "C", "D", "+"]));
+/**
+ * @param {string[]} operations
+ * @return {number}
+ */
+var calPoints = function (operations) {
+  let stack = [];
+  let total = 0;
+
+  for (let char of operations) {
+    if (!isNaN(char)) {
+      stack.push(parseInt(char));
+    } else if (char === "+") {
+      let a = stack[stack.length - 1];
+      let b = stack[stack.length - 2];
+      stack.push(a + b);
+    } else if (char === "D") {
+      stack.push(2 * stack[stack.length - 1]);
+    } else if (char === "C") {
+      stack.pop();
+    } else {
+      continue;
+    }
+  }
+  for (let num of stack) {
+    total += num;
+  }
+  return total;
+};
+
+console.log(calPoints(["5", "2", "C", "D", "+"]));
