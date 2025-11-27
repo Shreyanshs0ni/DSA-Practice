cfd9c226cf2dc00280c49a9b972a7c6dc1c54dcd
Shreyansh <shreyanshsoni07@gmail.com>
2025-11-27 18:57:06 +0530

    Jewels and stones done

---
+++ Arrays/easy/23.JewelsAndStones/app.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} jewels
+ * @param {string} stones
+ * @return {number}
+ */
+var numJewelsInStones = function (jewels, stones) {
+  let set = new Set();
+  let count = 0;
+  for (let i = 0; i < jewels.length; i++) {
+    set.add(jewels[i]);
+  }
+  for (let i = 0; i < stones.length; i++) {
+    if (set.has(stones[i])) {
+      count++;
+    }
+  }
+  return count;
+};
+
+console.log(numJewelsInStones("aA", "aAAbbbb"));
