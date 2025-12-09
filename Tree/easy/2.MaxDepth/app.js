893d89f10d8411e59ad8d58497ae20735b4a28ce
Shreyansh <shreyanshsoni07@gmail.com>
2025-12-09 23:16:35 +0530

    Max Depth done

---
+++ Tree/easy/2.MaxDepth/app.js
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function (root) {
+  if (!root) return 0;
+  const lDepth = maxDepth(root.left);
+  const rDepth = maxDepth(root.right);
+
+  return 1 + Math.max(lDepth, rDepth);
+};
