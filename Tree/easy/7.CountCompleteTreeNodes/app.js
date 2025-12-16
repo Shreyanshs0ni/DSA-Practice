daecad73c6c05a2bcb3cea2ac1108a22a600ff99
Shreyansh <shreyanshsoni07@gmail.com>
2025-12-16 22:18:53 +0530

    222. Count Complete Tree Nodes Done

---
+++ Tree/easy/7.CountCompleteTreeNodes/app.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var countNodes = function (root) {
+    if (!root) return 0;
+    let lCount = countNodes(root.left);
+    let rCount = countNodes(root.right);
+    return lCount + rCount + 1;
+}
