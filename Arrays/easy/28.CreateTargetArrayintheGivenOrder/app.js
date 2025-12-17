a5e51625f80600c02041ca99da96e49d2a5c162e
Shreyansh <shreyanshsoni07@gmail.com>
2025-12-17 23:49:17 +0530

    1389. Create Target Array in the Given Order Done

---
+++ Arrays/easy/28.CreateTargetArrayintheGivenOrder/app.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @param {number[]} index
+ * @return {number[]}
+ */
+var createTargetArray = function (nums, index) {
+  const target = [];
+  for (let i = 0; i < nums.length; i++) {
+    target.splice(index[i], 0, nums[i]);
+  }
+  return target;
+};
+
+console.log(createTargetArray([0, 1, 2, 3, 4], [0, 1, 2, 2, 1]));
+
+// Example 1:
+
+// Input: nums = [0,1,2,3,4], index = [0,1,2,2,1]
+// Output: [0,4,1,3,2]
+// Explanation:
+// nums       index     target
+// 0            0        [0]
+// 1            1        [0,1]
+// 2            2        [0,1,2]
+// 3            2        [0,1,3,2]
+// 4            1        [0,4,1,3,2]
