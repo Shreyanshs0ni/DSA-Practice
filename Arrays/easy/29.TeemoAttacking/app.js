77c6d45f03b1fc89e587fac6262921d8335cdd43
Shreyansh <shreyanshsoni07@gmail.com>
2025-12-31 09:25:40 +0530

    495. Teemo Attacking Done

---
+++ Arrays/easy/29.TeemoAttacking/app.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} timeSeries
+ * @param {number} duration
+ * @return {number}
+ */
+var findPoisonedDuration = function (timeSeries, duration) {
+  if (timeSeries.length === 0) return 0;
+  let total = 0;
+  for (let i = 0; i < timeSeries.length - 1; i++) {
+    total += Math.min(timeSeries[i + 1] - timeSeries[i], duration);
+  }
+  return total + duration;
+};
+
+console.log(findPoisonedDuration([1, 4], 2));
+console.log(findPoisonedDuration([1, 2], 2));
