3a7432bcae29f3a3082380dcec87ba730933da14
Shreyansh <shreyanshsoni07@gmail.com>
2026-01-16 23:02:44 +0530

    1431. Kids With the Greatest Number of Candies done

---
+++ Arrays/easy/38.KidsWiththeGreatestNumberofCandies/app1.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function (candies, extraCandies) {
+  let max = Math.max(...candies);
+  let res = [];
+  for (let candy of candies) {
+    if (candy + extraCandies >= max) {
+      res.push(true);
+    } else {
+      res.push(false);
+    }
+  }
+
+  return res;
+};
+
+console.log(kidsWithCandies([2, 3, 5, 1, 3], 3)); //[true,true,true,false,true]
+console.log(kidsWithCandies([4, 2, 1, 1, 2], 1)); //[true, false, false, false, false];
+console.log(kidsWithCandies([12, 1, 12], 10)); //[true,false,true]
