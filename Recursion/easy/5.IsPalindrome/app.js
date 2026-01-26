34c71c89841f3901680990468158c743c184e8e5
Shreyansh <shreyanshsoni07@gmail.com>
2026-01-26 21:31:07 +0530

    125. Valid Palindrome (recursion done)

---
+++ Recursion/easy/5.IsPalindrome/app.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s) {
+  let cleanS = "";
+  let alphabets = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let set = new Set([...alphabets]);
+  for (let letter of s) {
+    if (set.has(letter)) {
+      cleanS += letter.toLowerCase();
+    }
+  }
+  const check = (left, right) => {
+    if (left >= right) return true;
+    if (cleanS[left] !== cleanS[right]) return false;
+    return check(left + 1, right - 1);
+  };
+  return check(0, cleanS.length - 1);
+};
+console.log(isPalindrome("A man, a plan, a canal: Panama")); //true
+console.log(isPalindrome("race a car")); //false
+console.log(isPalindrome(" ")); //true
