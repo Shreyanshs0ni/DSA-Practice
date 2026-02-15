23ea74edd767655b6e983426da31e548f593c0e9
Shreyansh Soni <shreyanshsoni07@gmail.com>
2026-02-15 16:26:31 +0530

    Do 832. Flipping an Image Optimally

---
+++ Arrays/easy/832.FlippinganImage/app.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[][]} image
+ * @return {number[][]}
+ */
+var flipAndInvertImage = function (image) {
+  for (let i = 0; i < image.length; i++) {
+    image[i].reverse();
+    for (let j = 0; j < image[i].length; j++) {
+      image[i][j] = image[i][j] ^ 1;
+    }
+  }
+  return image;
+};
+
+console.log(
+  flipAndInvertImage([
+    [1, 1, 0],
+    [1, 0, 1],
+    [0, 0, 0],
+  ]),
+); //[[1,0,0],[0,1,0],[1,1,1]]
+console.log(
+  flipAndInvertImage([
+    [1, 1, 0, 0],
+    [1, 0, 0, 1],
+    [0, 1, 1, 1],
+    [1, 0, 1, 0],
+  ]),
+); //[[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
