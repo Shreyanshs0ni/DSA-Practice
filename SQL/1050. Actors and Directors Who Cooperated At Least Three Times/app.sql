0f1afab34be07b1edb1d70c3d0a7aba06c752b8e
Shreyansh Soni <shreyanshsoni07@gmail.com>
2026-03-16 20:50:06 +0530

    Do 1050. Actors and Directors Who Cooperated At Least Three Times optimally

--- SQL/1050. Actors and Directors Who Cooperated At Least Three Times/app.sql
+++ SQL/1050. Actors and Directors Who Cooperated At Least Three Times/app.sql
@@ -1,5 +1,8 @@
 # Write your MySQL query statement below
-SELECT class
-FROM Courses
-GROUP BY class
-HAVING COUNT(class) >= 5;
+SELECT MAX(num) AS num
+FROM (
+    SELECT num
+    FROM MyNumbers
+    GROUP BY num
+    HAVING COUNT(num) = 1
+) AS unique_numbers;
