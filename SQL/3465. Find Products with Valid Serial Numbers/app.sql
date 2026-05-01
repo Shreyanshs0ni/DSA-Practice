0bd586be7453aa2d8a85957d43c6c9cdde26b370
Shreyansh Soni <shreyanshsoni07@gmail.com>
2026-05-01 22:48:15 +0530

    Do 3465. Find Products with Valid Serial Numbers Optimally

--- SQL/3465. Find Products with Valid Serial Numbers/app.sql
+++ SQL/3465. Find Products with Valid Serial Numbers/app.sql
@@ -1,15 +1,9 @@
 # Write your MySQL query statement below
-SELECT 
-    l.book_id, 
-    l.title, 
-    l.author, 
-    l.genre, 
-    l.publication_year, 
-    l.total_copies AS current_borrowers
-FROM library_books l
-JOIN borrowing_records b
-USING (book_id)
-WHERE b.return_date IS NULL
-GROUP BY l.book_id
-HAVING COUNT(*) = l.total_copies
-ORDER BY current_borrowers DESC, l.title;
+SELECT *
+FROM products
+WHERE REGEXP_LIKE(
+    description,
+    '\\bSN[0-9]{4}-[0-9]{4}\\b',
+    'c'
+)
+ORDER BY product_id;
