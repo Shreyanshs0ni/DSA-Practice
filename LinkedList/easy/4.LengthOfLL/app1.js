064d53892be870901dab23ba815689a50ca4c9b7
Shreyansh <shreyanshsoni07@gmail.com>
2026-06-13 12:54:29 +0530

    Implement find the length of LinkedList

---
+++ LinkedList/easy/4.LengthOfLL/app1.js
@@ -0,0 +1,41 @@
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+function arrToLL(arr) {
+  if (arr.length === 0) return null;
+  const head = new Node(arr[0]);
+  let current = head;
+  for (let i = 1; i < arr.length; i++) {
+    current.next = new Node(arr[i]);
+    current = current.next;
+  }
+  return head;
+}
+
+function printLL(head) {
+  let res = "";
+  while (head !== null) {
+    res += head.value + "->";
+    head = head.next;
+  }
+  res += "null";
+  console.log(res);
+}
+
+function findLength(head) {
+  let count = 0;
+  while (head !== null) {
+    count++;
+    head = head.next;
+  }
+  return count;
+}
+
+let a = [1, 2, 3, 4, 5, 6, 7, 8];
+const head = arrToLL(a);
+printLL(head);
+console.log(findLength(head));
