56124bce5d1895b30b422cdb750612e50d0a3fe4
Shreyansh <shreyanshsoni07@gmail.com>
2026-06-17 12:34:31 +0530

    Implement Sort a stack recursively

---
+++ Recursion/medium/5.SortAStack/app1.js
@@ -0,0 +1,26 @@
+function sortedInsert(stack, element) {
+  if (stack.length === 0 || stack[stack.length - 1] <= element) {
+    stack.push(element);
+    return;
+  }
+  const top = stack.pop();
+  sortedInsert(stack, element);
+  stack.push(top);
+}
+
+function sortStack(stack) {
+  if (stack.length === 0) {
+    return;
+  }
+  const top = stack.pop();
+  sortStack(stack);
+  sortedInsert(stack, top);
+}
+
+const stack = [30, -5, 18, 14, -3]; // -3 is at the top
+
+console.log("Before:", stack);
+
+sortStack(stack);
+
+console.log("After:", stack);
