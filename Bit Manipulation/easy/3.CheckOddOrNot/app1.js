47792887c50b7c703dde63a376c22cb50f3e7086
Shreyansh <shreyanshsoni07@gmail.com>
2026-06-18 12:51:05 +0530

    Implement Odd Or not

---
+++ Bit Manipulation/easy/3.CheckOddOrNot/app1.js
@@ -0,0 +1,8 @@
+function oddOrNot(a) {
+  return (a & 1) === 1;
+}
+console.log(oddOrNot(2));
+console.log(oddOrNot(3));
+console.log(oddOrNot(4));
+console.log(oddOrNot(5));
+console.log(oddOrNot(6));
