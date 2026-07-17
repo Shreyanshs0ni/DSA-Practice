c5a941a6e81d899282be0ae21ae61c564fa904fd
Shreyansh <shreyanshsoni07@gmail.com>
2026-07-17 19:19:18 +0530

    Implement Climbing Stairs

---
+++ Dynamic Programming/easy/1.Climbing Stairs/app1.js
@@ -0,0 +1,18 @@
+function helper(dp, index) {
+  if (index === 0) return 1;
+  if (index === 1) return 1;
+
+  if (dp[index] !== -1) return dp[index];
+
+  let left = helper(dp, index - 1);
+  let right = helper(dp, index - 2);
+  dp[index] = left + right;
+
+  return dp[index];
+}
+var climbStairs = function (index) {
+  let dp = new Array(index + 1).fill(-1);
+  return helper(dp, index);
+};
+
+console.log(climbStairs(3));
