81da138b7a1deba6d898d945781144ed08fdfd88
Shreyansh <shreyanshsoni07@gmail.com>
2026-09-02 22:26:25 +0530

    Implement 3875. Construct Uniform Parity Array I

---
+++ Math/medium/3875. Construct Uniform Parity Array I/app.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums1
+ * @return {boolean}
+ */
+var uniformArray = function (nums1) {
+  return true;
+};
+
+console.log(uniformArray([2, 3]));
+console.log(uniformArray([4, 6]));
